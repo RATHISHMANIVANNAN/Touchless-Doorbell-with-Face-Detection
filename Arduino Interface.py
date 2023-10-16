914baafe215912a0feb43b6c7488e51ab95552e7
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2023-10-16 14:27:57 +0530

    Create Arduino Interface.py

---
+++ Arduino Interface.py
@@ -0,0 +1,11 @@
+import serial
+import time
+from face_recognise import detect_face
+ArduinoSerial = serial.Serial(port='COM3', baudrate=9600)
+time.sleep(2)
+
+while True:
+  detection = ArduinoSerial.readline().decode().strip()
+  print(detection)
+  if 'Motion Detected' in detection:
+    detect_face()
