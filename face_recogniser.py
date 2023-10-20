a3b9bf078a1ed2a703e573dba6204b22d849cbc2
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2023-10-20 10:28:04 +0530

    Update face_recogniser.py

--- face_recogniser.py
+++ face_recogniser.py
@@ -1,19 +1,30 @@
 import cv2
 import pickle
+from gtts import gTTS
+import pygame
+
+
+def say_my_name(person):
+    tts = gTTS(person+" has arrived at your doorstep", lang='en') #creates text to speech object in english
+    tts.save("labels.mp3")                                        #saves the audio in the file label.mp3
+    pygame.init()                                                 #initializes pygame library for audio playback
+    pygame.mixer.music.load("labels.mp3")                         #uploads the labels.mp3 file into pygame for audio play back
+    pygame.mixer.music.play()                                     #plays the loaded audio file
 
 def detect_face():
-    video = cv2.VideoCapture(0)
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    flag=True
+    video = cv2.VideoCapture(0)                                   #initializes video capture object 
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") #loads pre trianed classifier
 
-    recognizer = cv2.face_LBPHFaceRecognizer.create()
-    recognizer.read("trainner.yml")
+    recognizer = cv2.face_LBPHFaceRecognizer.create()             #initializes LBHPF recognizer
+    recognizer.read("trainner.yml")                               #loads previously trained trainner.yml file
 
-    labels = {}
-    with open("labels.pickle", 'rb') as f:
-        og_labels = pickle.load(f)
+    labels = {}                                                   #initializing an empty set for storing labels
+    with open("labels.pickle", 'rb') as f:                        #is used to open the labels.pickle and loads the labels and populates them
+        og_labels = pickle.load(f)                            
         labels = {v: k for k, v in og_labels.items()}
 
-    while True:
+    while True:                                                  
         check, frame = video.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -22,12 +33,19 @@ def detect_face():
         for x, y, w, h in faces:
             face_roi = gray[y:y + h, x:x + w]
 
-            ID, conf = recognizer.predict(face_roi)
+            ID, conf = recognizer.predict(face_roi)               #for each face calculates ID and confidence
 
             if conf >= 115 and conf <= 205:
-                label = labels.get(ID, "Anonymous")
+                label = labels.get(ID, "Anonymous")          
+                if flag:
+                    say_my_name(label)
+                    flag=False
+
             else:
                 label = "Anonymous"
+                if flag:
+                    say_my_name(label)
+                    flag=False
 
             cv2.putText(frame, label, (x - 10, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (18, 5, 255), 2, cv2.LINE_AA)
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 4)
@@ -39,5 +57,7 @@ def detect_face():
 
     video.release()
     cv2.destroyAllWindows()
-
-
+'''
+if __name__ == "__main__" :
+    detect_face()
+'''
