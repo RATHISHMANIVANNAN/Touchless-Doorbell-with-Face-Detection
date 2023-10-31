4c223051a6af326878f49eb550de65a70beec129
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2023-10-31 21:17:34 +0530

    Code for face recognition module

--- face_recogniser.py
+++ face_recogniser.py
@@ -3,46 +3,44 @@ import pickle
 from gtts import gTTS
 import pygame
 
-
 def say_my_name(person):
-    tts = gTTS(person+" has arrived at your doorstep", lang='en') #creates text to speech object in english
-    tts.save("labels.mp3")                                        #saves the audio in the file label.mp3
-    pygame.init()                                                 #initializes pygame library for audio playback
-    pygame.mixer.music.load("labels.mp3")                         #uploads the labels.mp3 file into pygame for audio play back
-    pygame.mixer.music.play()                                     #plays the loaded audio file
+    tts = gTTS(person+" has arrived at your doorstep", lang='en')           #creates text to speech object in english
+    tts.save("labels.mp3")                                                  #saves the audio in the file label.mp3
+    pygame.init()                                                           #initializes pygame library for audio playback
+    pygame.mixer.music.load("labels.mp3")                                   #uploads the labels.mp3 file into pygame for audio play back
+    pygame.mixer.music.play()                                               #plays the loaded audio file
 
 def detect_face():
     flag=True
-    video = cv2.VideoCapture(0)                                   #initializes video capture object 
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") #loads pre trianed classifier
+    video = cv2.VideoCapture(0)                                             #initializes video capture object 
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  #loads pre trianed classifier
+    recognizer = cv2.face_LBPHFaceRecognizer.create()                       #initializes LBHPF recognizer
+    recognizer.read("trainner.yml")                                         #loads previously trained trainner.yml file
 
-    recognizer = cv2.face_LBPHFaceRecognizer.create()             #initializes LBHPF recognizer
-    recognizer.read("trainner.yml")                               #loads previously trained trainner.yml file
-
-    labels = {}                                                   #initializing an empty set for storing labels
-    with open("labels.pickle", 'rb') as f:                        #is used to open the labels.pickle and loads the labels and populates them
+    labels = {}                                                             #initializing an empty set for storing labels
+    with open("labels.pickle", 'rb') as f:                                  #is used to open the labels.pickle and loads the labels and populates them
         og_labels = pickle.load(f)                            
         labels = {v: k for k, v in og_labels.items()}
 
     while True:                                                  
-        check, frame = video.read()
+        check, frame = video.read()                                                
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+        faces = cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)     #converting image to black and white 
 
-        for x, y, w, h in faces:
+        for x, y, w, h in faces:                                                    
             face_roi = gray[y:y + h, x:x + w]
 
-            ID, conf = recognizer.predict(face_roi)               #for each face calculates ID and confidence
+            ID, conf = recognizer.predict(face_roi)                          #for each face calculates ID and confidence
 
-            if conf >= 115 and conf <= 205:
+            if conf >= 115 and conf <= 205:                                  #confidence is used to detect the face
                 label = labels.get(ID, "Anonymous")          
                 if flag:
                     say_my_name(label)
                     flag=False
 
             else:
-                label = "Anonymous"
+                label = "Anonymous"                                          #if no face is identified it displays anonymous
                 if flag:
                     say_my_name(label)
                     flag=False
