679ec78bba6e6b33cde7dd798bbf1e3a604bf82e
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2023-10-31 21:19:08 +0530

    Code for face detection

--- face_detection.py
+++ face_detection.py
@@ -3,30 +3,30 @@ import cv2
 # Load the pre-trained face detection classifier (Haar Cascade)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Create a VideoCapture object for accessing the webcam (0 for default webcam)
+# Create a VideoCapture object for accessing the webcam
 video_capture = cv2.VideoCapture(0)
 
 while True:
-    # Read a frame from the webcam
-    ret, frame = video_capture.read()
+   
+    ret, frame = video_capture.read()      # Read a frame from the webcam
 
-    # Convert the frame to grayscale for face detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+   
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # Convert the frame to grayscale for face detection
 
-    # Detect faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))   # Detect faces in the grayscale frame
 
     # Draw rectangles around detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw a green rectangle around the face
 
-    # Display the frame with detected faces
-    cv2.imshow('Face Detection', frame)
+    
+    cv2.imshow('Face Detection', frame)   # Display the frame with detected faces
 
-    # Exit the loop when 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+   
+    if cv2.waitKey(1) & 0xFF == ord('q'):   # Exit the loop when 'q' key is pressed
         break
 
-# Release the VideoCapture and close all OpenCV windows
-video_capture.release()
+
+video_capture.release()  # Release the VideoCapture and close all OpenCV windows
 cv2.destroyAllWindows()
